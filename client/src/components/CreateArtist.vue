6356224f5d98ee4a4ea4f5e20e18c9ea615de8fc
faudaux <fedeab2000@gmail.com>
2023-06-03 00:19:24 -0300

    set up vue project

---
+++ client/src/components/CreateArtist.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" v-bind="artist_name">
+    <button type="submit">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            artist_name: null
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
